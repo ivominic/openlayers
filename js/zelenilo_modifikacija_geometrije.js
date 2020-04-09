dc06e4eb039b02429d910fae049ac6f15714bf50
Ivo Minic <ivo.minic@gmail.com>
2020-04-09 08:52:17 +0200

    Povezani eventi sa kontrolama

--- js/zelenilo_modifikacija_geometrije.js
+++ js/zelenilo_modifikacija_geometrije.js
@@ -1,86 +1,89 @@
-import 'ol/ol.css';
-import {
-  Map,
-  View
-} from 'ol/index';
-import GeoJSON from 'ol/format/GeoJSON';
-import {
-  Modify,
-  Select,
-  Draw,
-  Snap
-} from 'ol/interaction';
-import {
-  Tile as TileLayer,
-  Vector as VectorLayer
-} from 'ol/layer';
-import {
-  OSM,
-  Vector as VectorSource
-} from 'ol/source';
-import {
-  useGeographic
-} from 'ol/proj';
+import "ol/ol.css";
+import { Map, View } from "ol/index";
+import GeoJSON from "ol/format/GeoJSON";
+import { Modify, Select, Draw, Snap } from "ol/interaction";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+import { OSM, Vector as VectorSource } from "ol/source";
+import { useGeographic } from "ol/proj";
 
 useGeographic();
 
 console.log(location.origin);
 
+/*document.querySelector("#podloga_osm").addEventListener("click", alert("osm"));
+document.querySelector("#podloga_topo").addEventListener("click", alert("topo"));
+document.querySelector("#podloga_satelit").addEventListener("click", alert("satelit"));
+document.querySelector("#podloga_osm").innerHTML = "";*/
+/*document.querySelector("#podloga_topo").click(functon() {
+  alert("topo");
+});*/
+
+document.querySelector("#pan").addEventListener("click", pan);
+document.querySelector("#odaberi").addEventListener("click", odaberi);
+document.querySelector("#dodaj").addEventListener("click", dodaj);
+document.querySelector("#izbrisi").addEventListener("click", izbrisi);
+document.querySelector("#izmijeni").addEventListener("click", izmijeni);
+document.querySelector("#atributi").addEventListener("click", atributi);
+document.querySelector("#marker").addEventListener("click", marker);
+document.querySelector("#pretraga").addEventListener("click", pretraga);
+document.querySelector("#podloga_osm").addEventListener("click", pan);
+document.querySelector("#podloga_topo").addEventListener("click", pan);
+
 var source = new VectorSource({
-  url: 'http://localhost/gis/staze.geojson',
+  url: "http://localhost/gis/staze.geojson",
   //url: 'https://openlayers.org/en/latest/examples/data/geojson/countries.geojson',
-  format: new GeoJSON()
+  format: new GeoJSON(),
 });
 
 var map = new Map({
-  target: 'map',
+  target: "map",
   layers: [
     new TileLayer({
-      source: new OSM()
+      source: new OSM(),
     }),
     new VectorLayer({
-      source: source
-    })
+      source: source,
+    }),
   ],
   view: new View({
     center: [19.2629, 42.43862],
-    zoom: 18
-  })
+    zoom: 18,
+  }),
 });
 
 var select = new Select();
 
 var modify = new Modify({
-  features: select.getFeatures()
+  features: select.getFeatures(),
 });
 
 var draw = new Draw({
-  type: 'Polygon',
-  source: source
+  type: "Polygon",
+  source: source,
 });
 
 var snap = new Snap({
-  source: source
+  source: source,
 });
 
 function removeInteractions() {
   map.removeInteraction(modify);
   map.removeInteraction(select);
   map.removeInteraction(draw);
-  map.removeInteraction(select);
+  map.removeInteraction(snap);
 }
 
-var mode = document.getElementById('mode');
+var mode = document.getElementById("mode");
 
 function onChange() {
   removeInteractions();
   switch (mode.value) {
-    case 'draw': {
+    case "draw": {
       map.addInteraction(draw);
       map.addInteraction(snap);
       break;
     }
-    case 'modify': {
+    case "modify": {
       map.addInteraction(select);
       map.addInteraction(modify);
       map.addInteraction(snap);
@@ -91,15 +94,41 @@ function onChange() {
     }
   }
 }
-mode.addEventListener('change', onChange);
+mode.addEventListener("change", onChange);
 onChange();
 
-var pomocna;
-select.on('select', function (e) {
-  pomocna = e;
-  console.log(e.selected[0].getProperties()["id"]);
-  // document.getElementById('status').innerHTML = '&nbsp;' +
-  //     e.target.getFeatures().getLength() +
-  //     ' selected features (last operation selected ' + e.selected.length +
-  //     ' and deselected ' + e.deselected.length + ' features)';
-});
+function pan() {
+  console.log("PAN");
+}
+
+function odaberi() {
+  console.log("ODABERI");
+}
+
+function dodaj() {
+  console.log("DODAJ");
+}
+
+function izbrisi() {
+  console.log("IZBRISI");
+}
+
+function izmijeni() {
+  console.log("IZMIJENI");
+}
+
+function atributi() {
+  console.log("atributi");
+}
+
+function atributi() {
+  console.log("atributi");
+}
+
+function marker() {
+  console.log("marker");
+}
+
+function pretraga() {
+  console.log("pretraga");
+}
